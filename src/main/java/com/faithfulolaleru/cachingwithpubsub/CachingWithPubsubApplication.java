3ca9ccc021369ed0b4dcc75dbd3e50905a0679c7
Faithful <faithfulolaleru09@gmail.com>
2023-03-20 13:26:49 +0100

    initial commit

---
+++ src/main/java/com/faithfulolaleru/cachingwithpubsub/CachingWithPubsubApplication.java
@@ -0,0 +1,13 @@
+package com.faithfulolaleru.cachingwithpubsub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CachingWithPubsubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CachingWithPubsubApplication.class, args);
+	}
+
+}
